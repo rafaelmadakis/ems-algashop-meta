791fe78dab6d106264b916e31c40c92b52a1b3e8
Rafael Madakis <rafaelmadakis@gmail.com>
2025-10-31 02:03:06 -0300

    Aprimorando Exceptions com Factory Method

--- microservices/ordering/src/main/java/com/algaworks/algashop/ordering/domain/entity/Order.java
+++ microservices/ordering/src/main/java/com/algaworks/algashop/ordering/domain/entity/Order.java
@@ -109,20 +109,20 @@ public class Order {
   }
 
   public void place() {
-    Objects.requireNonNull(this.shipping());
-    Objects.requireNonNull(this.billing());
-    Objects.requireNonNull(this.expectedDeliveryDate());
-    Objects.requireNonNull(this.shippingCost());
-    Objects.requireNonNull(this.paymentMethod());
-    Objects.requireNonNull(this.items());
+//    Objects.requireNonNull(this.shipping());
+//    Objects.requireNonNull(this.billing());
+//    Objects.requireNonNull(this.expectedDeliveryDate());
+//    Objects.requireNonNull(this.shippingCost());
+//    Objects.requireNonNull(this.paymentMethod());
+//    Objects.requireNonNull(this.items());
 
-    if (this.items().isEmpty()) {
-      throw new OrderCannotBePlacedException(this.id());
-    }
+  this.verifyIfCanChangeToPlaced();
 
     this.setPlacedAt(OffsetDateTime.now());
     this.changeStatus(OrderStatus.PLACED);
   }
+
+
 
   public void markAsPaid() {
     this.setPaidAt(OffsetDateTime.now());
@@ -253,6 +253,27 @@ public class Order {
 
     }
     this.setStatus(newStatus);
+  }
+
+  private void verifyIfCanChangeToPlaced() {
+    if (this.shipping() == null) {
+      throw  OrderCannotBePlacedException.noShippingInfo(this.id());
+    }
+    if (this.billing() == null) {
+      throw  OrderCannotBePlacedException.noBillingInfo(this.id());
+    }
+    if (this.expectedDeliveryDate() == null) {
+      throw  OrderCannotBePlacedException.noPaymentMethod(this.id());
+    }
+    if (this.shippingCost() == null) {
+      throw  OrderCannotBePlacedException.invalidShippingCost(this.id());
+    }
+    if (this.expectedDeliveryDate() == null) {
+      throw  OrderCannotBePlacedException.invalidExpectedDeliveryDate(this.id());
+    }
+    if (this.items() == null || this. items().isEmpty()) {
+      throw  OrderCannotBePlacedException.noItems(this.id());
+    }
   }
 
   private void setId(OrderId id) {
--- microservices/ordering/src/main/java/com/algaworks/algashop/ordering/domain/exception/ErrorMessages.java
+++ microservices/ordering/src/main/java/com/algaworks/algashop/ordering/domain/exception/ErrorMessages.java
@@ -1,7 +1,6 @@
 package com.algaworks.algashop.ordering.domain.exception;
 
 public class ErrorMessages {
-
   public static final String VALIDATION_ERROR_EMAIL_IS_INVALID = "Email is invalid";
   public static final String VALIDATION_ERROR_BIRTHDATE_MUST_IN_PAST = "BirthDate must be a past date";
   public static final String VALIDATION_ERROR_FULLNAME_IS_NULL = "FullName cannot be null";
@@ -11,10 +10,24 @@ public class ErrorMessages {
 
   public static final String ERROR_ORDER_STATUS_CANNOT_BE_CHANGED = "Cannot change order %s status from %s to %s";
 
-  public static final String ERROR_ORDER__DELIVERY_DATE_CANNOT_BE_IN_THE_PAST = "Order %s expected delivery date cannot be in the past";
+  public static final String ERROR_ORDER_DELIVERY_DATE_CANNOT_BE_IN_THE_PAST
+      = "Order %s expected delivery date cannot be in the past";
 
-  public static final String ERROR_ORDER_CANNOT_BE_PLACED_HAS_NOT_ITEMS = "Order %s cannot be closed, it has no items";
+  public static final String ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_ITEMS
+      = "Order %s cannot be placed, it has no items";
 
+  public static final String ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_SHIPPING_INFO
+      = "Order %s cannot be placed, it has no shipping info";
 
+  public static final String ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_BILLING_INFO
+      = "Order %s cannot be placed, it has no billing info";
 
+  public static final String ERROR_ORDER_CANNOT_BE_PLACED_INVALID_SHIPPING_COST
+      = "Order %s cannot be placed, it has no shipping cost";
+
+  public static final String ERROR_ORDER_CANNOT_BE_PLACED_INVALID_DELIVERY_DATE
+      = "Order %s cannot be placed, it has no delivery date";
+
+  public static final String ERROR_ORDER_CANNOT_BE_PLACED_HAS_NO_PAYMENT_METHOD
+      = "Order %s cannot be placed, it has no payment method";
 }
